8e54877b8f29f56b297f5a4558b91488e21351e0
julien oving <37832@ma-web.nl>
2023-12-08 18:51:15 +0100

    flipper workin

--- My project/Assets/script/flipper.cs
+++ My project/Assets/script/flipper.cs
@@ -5,16 +5,14 @@ using UnityEngine;
 public class flipper : MonoBehaviour
 {
     [SerializeField] private float timer = 0f;
+    [SerializeField] private GameObject leftWall;
+    [SerializeField] private GameObject rightWall;
     private float delay = 10f;
-    [SerializeField] private GameObject left-wall;
-    [SerializeField] private GameObject right-wall;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private bool CanClick = true;
+    [SerializeField] private bool HasFood = false;
+    
+    [SerializeField] private Transform ThisTransform;
+    [SerializeField] private Transform FoodTransform;
 
     // Update is called once per frame
     void Update()
@@ -25,8 +23,29 @@ public class flipper : MonoBehaviour
             //do iets
         }
 
-        if (Input.GetKeyDown("k")){
-            //doe iets
+        if (Input.GetKeyDown("k") && CanClick && HasFood){
+            transform.Rotate(0,0,-45);
+            rightWall.SetActive(false);
+            CanClick = false;
+        }
+        if (Input.GetKeyDown("d") && CanClick && HasFood){
+            transform.Rotate(0,0,45);
+            leftWall.SetActive(false);
+            CanClick = false;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "food"){
+            HasFood = true;
+            FoodTransform.parent = ThisTransform;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "food" && HasFood){
+            HasFood = false;
+            FoodTransform.parent = null;
         }
     }
 }
